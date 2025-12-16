d20592c2a6c24b38ef8f30b59aebffa4df6adfa5
Mibess <mibexzooo@gmail.com>
2025-12-16 15:11:18 -0300

    feat: refact header

--- ead-frontend/src/app/pages/home/home.page/home.page.ts
+++ ead-frontend/src/app/pages/home/home.page/home.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { Header } from "../../../shared/header/header";
 
 @Component({
   selector: 'app-home.page',
-  imports: [ RouterLink],
+  imports: [RouterLink, Header],
   templateUrl: './home.page.html',
   styleUrl: './home.page.css',
 })
